e233b63a641cf980806f850f93f731cf0cdea612
Chinar Joshi <chinarjoshi7@gmail.com>
2021-05-26 18:35:22 -0400

    Added Session ID validation function

--- src/polar_rover/sessions/forms.py
+++ src/polar_rover/sessions/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    id = StringField('Session ID',
+    name = StringField('Session ID',
                            validators=[DataRequired(), Length(min=5, max=50)])
     password = PasswordField('Session Password',
                            validators=[DataRequired(), Length(min=3, max=30)])
@@ -13,11 +13,17 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login to a Session')
 
+
 class RegistrationForm(FlaskForm):
-    id = StringField('Session ID',
+    name = StringField('Session ID',
                            validators=[DataRequired(), Length(min=5, max=50)])
     password = PasswordField('Session Password',
                            validators=[DataRequired(), Length(min=3, max=30)])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), Length(min=3, max=30)])
     submit = SubmitField('Register a Session')
+
+    def validate_name(self, name):
+        session = Session.query.filter_by(name=name.data).first()
+        if session:
+            raise ValidationError('Session ID is taken.')
