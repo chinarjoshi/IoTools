45c20ad3e8cefd4d40f6904773ce88adf8d9ddb0
Chinar Joshi <chinarjoshi7@gmail.com>
2021-05-25 21:25:39 -0400

    Updated docstrings.

--- src/polar_rover/routes.py
+++ src/polar_rover/routes.py
@@ -19,10 +19,12 @@ KEY = '6,wD-Ak]^wzWe@G'  # expected key for verification
 
 @polar_rover.app.route('/')
 def home():
+    """Home page."""
     return render_template('home.html')
 
 @polar_rover.app.route('/about')
 def about():
+    """About page."""
     return render_template('about.html', title='About')
 
 @polar_rover.app.route('/data')
@@ -30,12 +32,13 @@ def data() -> Response:
     """Read the data from the cvs file.
 
     Returns
-        data_json (json) : a json containing the information of the file
+        data_json (Response): json containing the information of the file
     """
     return jsonify({'data': pd.read_csv(CSV_STREAM).to_dict()})
 
 @polar_rover.app.route('/update', methods=['POST'])
 def update() -> Output:
+    """Update csv values from request."""
     values = {}
     for field in 'key', 'probeTemp', 'bmeTemp', 'bmeHumid', 'bmeAlt', 'bmeAir':
         try:
@@ -46,12 +49,11 @@ def update() -> Output:
         csv_file.write(', '.join(values.items()))
     return Output('Successfully reset the data.', 201)
 
-# Reset database
 @polar_rover.app.route('/reset', methods=['POST'])
 def reset() -> Output:
+    """Reset database."""
     if request.form.get('key') != KEY:
         abort(403)
-    # Truncate the current csv file
     with open(CSV_STREAM, 'w') as csv_file:
         csv_file.write('Timestamp,Probe Temperature [C],BME Temperature [C],BME Humidity [%],BME Altitude [m],BME Air Pressure [hPA]\n')
     return Output('Successfully reset the data.', 201)
