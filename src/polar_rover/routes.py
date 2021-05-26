6e7f0a8fda699eddb590ce3305792a16442053a9
Chinar Joshi <chinarjoshi7@gmail.com>
2021-05-25 21:08:33 -0400

    Updated dependencies and route imports

--- src/polar_rover/routes.py
+++ src/polar_rover/routes.py
@@ -1,10 +1,9 @@
+import importlib.resources as pkg_resources
 from dataclasses import dataclass
 
 import pandas as pd
 from flask import Response, abort, jsonify, render_template, request
 
-from polar_rover import app
-import importlib.resources as pkg_resources
 import polar_rover
 
 
@@ -14,27 +13,28 @@ class Output():
     message: str
     return_code: int
 
-DATABASE_ROUTE = pkg_resources.open_text(polar_rover, 'data.csv')
+# CSV file is imported from package route as file stream
+CSV_STREAM = pkg_resources.open_text(polar_rover, 'data.csv')
 KEY = '6,wD-Ak]^wzWe@G'  # expected key for verification
 
-@app.route('/')
+@polar_rover.app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/about')
+@polar_rover.app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/data')
+@polar_rover.app.route('/data')
 def data() -> Response:
     """Read the data from the cvs file.
 
     Returns
         data_json (json) : a json containing the information of the file
     """
-    return jsonify({'data': pd.read_csv(DATABASE_ROUTE).to_dict()})
+    return jsonify({'data': pd.read_csv(CSV_STREAM).to_dict()})
 
-@app.route('/update', methods=['POST'])
+@polar_rover.app.route('/update', methods=['POST'])
 def update() -> Output:
     values = {}
     for field in 'key', 'probeTemp', 'bmeTemp', 'bmeHumid', 'bmeAlt', 'bmeAir':
@@ -42,16 +42,16 @@ def update() -> Output:
             values[field] = request.form[field]
         except:
             abort(404)
-    with open(DATABASE_ROUTE, 'a') as csv_file:
+    with open(CSV_STREAM, 'a') as csv_file:
         csv_file.write(', '.join(values.items()))
     return Output('Successfully reset the data.', 201)
 
 # Reset database
-@app.route('/reset', methods=['POST'])
+@polar_rover.app.route('/reset', methods=['POST'])
 def reset() -> Output:
     if request.form.get('key') != KEY:
         abort(403)
     # Truncate the current csv file
-    with open(DATABASE_ROUTE, 'w') as csv_file:
+    with open(CSV_STREAM, 'w') as csv_file:
         csv_file.write('Timestamp,Probe Temperature [C],BME Temperature [C],BME Humidity [%],BME Altitude [m],BME Air Pressure [hPA]\n')
     return Output('Successfully reset the data.', 201)
