e7c40331d281db4094e79c0f2f46e506f4bff3d3
Chinar Joshi <chinarjoshi7@gmail.com>
2021-05-25 20:33:57 -0400

    Refactored /data, /update, and /reset routes

--- src/polar_rover/routes.py
+++ src/polar_rover/routes.py
@@ -1,117 +1,55 @@
+from dataclasses import dataclass
+
+import pandas as pd
+from flask import Response, abort, jsonify, render_template, request
+
 from polar_rover import app
-from flask import abort, jsonify, render_template, request, url_for, Response
 
-import csv
-from datetime import datetime
 
-DATABASE_ROUTE = './data.csv'
+@dataclass(frozen=True)
+class Output():
+    """Inmutable class for containing return message and code."""
+    message: str
+    return_code: int
 
-def user_validation(key_in):
-    return key_in == KEY
-
-KEY = "6,wD-Ak]^wzWe@G"  # expected key for verification
+DATABASE_ROUTE = 'data.csv'
+KEY = '6,wD-Ak]^wzWe@G'  # expected key for verification
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/data")
+@app.route('/data')
 def data() -> Response:
-    """
-    Read the data from the cvs file.
+    """Read the data from the cvs file.
 
     Returns
         data_json (json) : a json containing the information of the file
     """
-    times = []
-    probe_temps = []
-    bme_temps = []
-    bme_humidity = []
-    bme_altitudes = []
-    bme_airpress = []
-    with open(DATABASE_ROUTE) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        line_count = 0
-        for row in csv_reader:
-            if line_count != 0:
-                times.append(row[0])
-                probe_temps.append(float(row[1]))
-                bme_temps.append(float(row[2]))
-                bme_humidity.append(float(row[3]))
-                bme_altitudes.append(float(row[4]))
-                bme_airpress.append(float(row[5]))
-            line_count += 1
-    data = {
-        "timestamps": times,
-        "probeTemp": probe_temps,
-        "bmeTemp": bme_temps,
-        "bmeHumid": bme_humidity,
-        "bmeAlt": bme_altitudes,
-        "bmeAir": bme_airpress,
-    }
-    data_json = jsonify({"data": data})
-    return data_json
+    return jsonify({'data': pd.read_csv(DATABASE_ROUTE).to_dict()})
 
-
-# Update the database
-@app.route("/update", methods=["POST"])
-def update():
-    r = request.form
-    # Validate the user
-    if r.get("key"):
-        if not user_validation(r.get("key")):
-            print("Couldn't validate user")
-            abort(403)
-    if r.get("probeTemp"):
-        probeTemp = r.get("probeTemp")
-    else:
-        abort(404)
-    if r.get("bmeTemp"):
-        bmeTemp = r.get("bmeTemp")
-    else:
-        abort(404)
-    if r.get("bmeHumid"):
-        bmeHumid = r.get("bmeHumid")
-    else:
-        abort(404)
-    if r.get("bmeAlt"):
-        bmeAlt = r.get("bmeAlt")
-    else:
-        abort(404)
-    if r.get("bmeAir"):
-        bmeAir = r.get("bmeAir")
-    else:
-        abort(404)
-    timestamp = datetime.now().strftime(
-        "%d/%m/%Y %H:%M:%S"
-    )  # get the current time of the data being pushed
-
+@app.route('/update', methods=['POST'])
+def update() -> Output:
+    values = {}
+    for field in 'key', 'probeTemp', 'bmeTemp', 'bmeHumid', 'bmeAlt', 'bmeAir':
+        try:
+            values[field] = request.form[field]
+        except:
+            abort(404)
     with open(DATABASE_ROUTE, 'a') as csv_file:
-        csv_file.write(
-            "{}, {}, {}, {}, {}, {}\n".format(
-                timestamp, probeTemp, bmeTemp, bmeHumid, bmeAlt, bmeAir
-            )
-        )
-
-    return "Successfully updated the data.", 201
-
+        csv_file.write(', '.join(values.items()))
+    return Output('Successfully reset the data.', 201)
 
 # Reset database
-@app.route("/reset", methods=["POST"])
-def reset():
-    r = request.form
-    # Validate the user
-    if r.get("key"):
-        if not user_validation(r.get("key")):
-            abort(403)
+@app.route('/reset', methods=['POST'])
+def reset() -> Output:
+    if request.form.get('key') != KEY:
+        abort(403)
     # Truncate the current csv file
-    with open("static/database/data.csv", "w") as csv_file:
-        csv_file.truncate()
-        csv_file.write(
-            "Timestamp,Probe Temperature [C],BME Temperature [C],BME Humidity [%],BME Altitude [m],BME Air Pressure [hPA]\n"
-        )
-    return "Successfully reset the data.", 201
+    with open(DATABASE_ROUTE, 'w') as csv_file:
+        csv_file.write('Timestamp,Probe Temperature [C],BME Temperature [C],BME Humidity [%],BME Altitude [m],BME Air Pressure [hPA]\n')
+    return Output('Successfully reset the data.', 201)
