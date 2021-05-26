daaaeb854e3e8aa52c71c4003064ece81d050f43
Chinar Joshi <chinarjoshi7@gmail.com>
2021-05-26 19:20:15 -0400

    Data file is now imported as pkg_resource rather than standard open() with importlib

--- src/polar_rover/main/utils.py
+++ src/polar_rover/main/utils.py
@@ -1,10 +1,17 @@
 from dataclasses import dataclass
 
+import polar_rover
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    import importlib_resources as pkg_resources
+
+CSV_ROUTE = pkg_resources.open_text(polar_rover, 'data.csv')
+KEY = '6,wD-Ak]^wzWe@G'  # expected key for verification
+
 @dataclass(frozen=True)
 class Output():
     """Inmutable class for containing return message and code."""
     message: str
     return_code: int
-
-CSV_ROUTE = '../static/data/data.csv'
-KEY = '6,wD-Ak]^wzWe@G'  # expected key for verification
