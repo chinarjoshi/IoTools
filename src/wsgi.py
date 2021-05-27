d92ea1a53c9ff5640e979b3edb2ed864f2f4e829
Chinar Joshi <chinarjoshi7@gmail.com>
2021-05-27 15:56:20 -0400

    Update wsgi

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,3 +1,13 @@
+import os
+dir, _ = os.path.split(__file__)
+
+file_path = os.path.join(dir, '..', 'Procfile')
+with open(file_path) as file:
+    file.read()
+
+
+
+
 from rover_polar import create_app
 
 app = create_app()
